6cf09ffca59acd8b6ee9571008815233626a0500
salamkayodeji <salamkayodeji@gmail.com>
2021-12-07 06:54:58 +0100

    Updates

---
+++ precache-manifest.2b150269bf6dae5c69116c08c9ee8c81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21579992f636cba060a172d524b312e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1ae062c015efaf0aebf4",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "b5dbefe07914bfb956e7",
+    "url": "./static/css/main.5ccb174b.chunk.css"
+  },
+  {
+    "revision": "1ae062c015efaf0aebf4",
+    "url": "./static/js/2.dc46387f.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.dc46387f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5dbefe07914bfb956e7",
+    "url": "./static/js/main.aaf6237e.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
